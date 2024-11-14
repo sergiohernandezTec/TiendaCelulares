9c379ed2469145524452aaeef2f19f9dc54bae4d
Jaime <A01426033@tec.mx>
2024-11-14 10:51:58 -0600

    clase comentario

---
+++ Comentario.cpp
@@ -0,0 +1,8 @@
+//
+// Created by jaime on 14/11/2024.
+//
+
+#include "Comentario.h"
+
+Comentario::Comentario(int pnt, string txt)
+    : puntaje(pnt), texto(txt) {}
---
+++ Comentario.h
@@ -0,0 +1,21 @@
+//
+// Created by jaime on 14/11/2024.
+//
+
+#ifndef COMENTARIO_H
+#define COMENTARIO_H
+#include <string>
+using namespace std;
+
+
+class Comentario {
+public:
+    int puntaje;
+    string texto;
+
+    Comentario(int pnt, string txt);
+};
+
+
+
+#endif //COMENTARIO_H
