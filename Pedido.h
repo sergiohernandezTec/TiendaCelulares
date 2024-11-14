c0bfed2fc9f0db5968eaa2620545a3d18f6589f0
Sebastian Diaz <A01424712@tec.mx>
2024-11-14 10:56:55 -0600

    Clase Pedido

---
+++ Pedido.h
@@ -0,0 +1,21 @@
+
+#ifndef PEDIDO_H
+#define PEDIDO_H
+#include <string>
+#include<vector>
+#include<Cliente.h>
+using namespace std;
+
+class Pedido:public Cliente{
+private:
+    int idPedido;
+    string fecha;
+    double total;
+    vector <Producto>productos;
+    Factura factura;
+    Envio envio;
+};
+
+
+
+#endif //PEDIDO_H
