33786b2288ba500ee54ea41fe120ee9082ca8054
Valeria Porcayo <A01424758@tec.mx>
2024-11-21 15:07:35 -0600

    Listo

---
+++ Carrito.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Valeria Porcayo on 21/11/24.
+//
+
+#include "Carrito.h"
---
+++ Carrito.h
@@ -0,0 +1,16 @@
+//
+// Created by Valeria Porcayo on 21/11/24.
+//
+
+#ifndef CARRITO_H
+#define CARRITO_H
+
+
+
+class Carrito {
+
+};
+
+
+
+#endif //CARRITO_H
